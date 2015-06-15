cafe2e6708928646e16950d65949214120ea27a9
arvinchen <arvinchen@22605c27-2254-48e0-b827-bdb5f8ab8418>
2015-06-15 04:44:26 +0000

    DES加密修正

--- trunk/EncryptAlgorithm/SymmetryDESDlg.cpp
+++ trunk/EncryptAlgorithm/SymmetryDESDlg.cpp
@@ -15,10 +15,10 @@ SymmetryDESDlg::SYMMETRY_TABLE_DES_TYPE
 SymmetryDESDlg::GetDESType()
 {
 	SYMMETRY_TABLE_DES_TYPE eDES = SYMMETRY_TABLE_DES_DES;
-	if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DES)))->GetCheck()) {
+	if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DES)))->GetCheck()) {
 		eDES = SYMMETRY_TABLE_DES_DES;
 	}
-	else if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_3DES)))->GetCheck()) {
+	else if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_3DES)))->GetCheck()) {
 		eDES = SYMMETRY_TABLE_DES_3DES;
 	}
 	else {
@@ -31,19 +31,19 @@ SymmetryDESDlg::SYMMETRY_TABLE_DESMODE_TYPE
 SymmetryDESDlg::GetDESModeType()
 {
 	SYMMETRY_TABLE_DESMODE_TYPE eDESMode = SYMMETRY_TABLE_DESMODE_NON;
-	if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESECB)))->GetCheck()) {
+	if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESECB)))->GetCheck()) {
 		eDESMode = SYMMETRY_TABLE_DESMODE_ECB;
 	}
-	else if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCBC)))->GetCheck()) {
+	else if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCBC)))->GetCheck()) {
 		eDESMode = SYMMETRY_TABLE_DESMODE_CBC;
 	}
-	else if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCFB)))->GetCheck()) {
+	else if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCFB)))->GetCheck()) {
 		eDESMode = SYMMETRY_TABLE_DESMODE_CFB;
 	}
-	else if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESOFB)))->GetCheck()) {
+	else if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESOFB)))->GetCheck()) {
 		eDESMode = SYMMETRY_TABLE_DESMODE_OFB;
 	}
-	else if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCTR)))->GetCheck()) {
+	else if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCTR)))->GetCheck()) {
 		eDESMode = SYMMETRY_TABLE_DESMODE_CTR;
 	}
 	else {
@@ -87,11 +87,12 @@ SymmetryDESDlg::OnInitCtrl()
 {
 	GetDlgItem(IDC_STATIC_SYMMETRYDES_INITIALVALUE)->SetWindowText(STATIC_CONST_CSTR_INITIALVECTORVALUE);
 	
-	GetDlgItem(IDC_EDIT_SYMMETRYDES_INITIALVALUE)->SetWindowText("mx111111");
-	GetDlgItem(IDC_EDIT_SYMMETRYDES_CIPHERKEY)->SetWindowText("mx123456");
+	GetDlgItem(IDC_EDIT_SYMMETRYDES_INITIALVALUE)->SetWindowText("mxEncrypt");
+	((CEdit*)GetDlgItem(IDC_EDIT_SYMMETRYDES_INITIALVALUE))->SetLimitText(0x08);
+	GetDlgItem(IDC_EDIT_SYMMETRYDES_CIPHERKEY)->SetWindowText("mengxiangEncryptDecrypt");
 
-	((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_3DES)))->SetCheck(BST_CHECKED);	//默认使用3DES加密
-	((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESECB)))->SetCheck(BST_CHECKED);	//默认使用ECB模式
+	((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_3DES)))->SetCheck(BST_CHECKED);	//默认使用3DES加密
+	((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESECB)))->SetCheck(BST_CHECKED);	//默认使用ECB模式
 }
 
 void 
@@ -104,7 +105,7 @@ SymmetryDESDlg::UpdateMixCtrl()
 void 
 SymmetryDESDlg::UpdateMixCtrlValue()
 {
-	if (BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCTR)))->GetCheck()) {
+	if (BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESCTR)))->GetCheck()) {
 		GetDlgItem(IDC_STATIC_SYMMETRYDES_INITIALVALUE)->SetWindowText(STATIC_CONST_CSTR_INITIALCOUNTVALUE);	
 	}
 	else {
@@ -115,10 +116,16 @@ SymmetryDESDlg::UpdateMixCtrlValue()
 void 
 SymmetryDESDlg::UpdateMixCtrlEnable()
 {
-	bool b3DES = BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_3DES)))->GetCheck()?true:false;
+	bool b3DES = BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_3DES)))->GetCheck()?true:false;
 	//3DES加密模式支持16个字符, DES加密模式支持8个字符
+	CEdit* pEdit = (CEdit*)GetDlgItem(IDC_EDIT_SYMMETRYDES_CIPHERKEY);
+	pEdit->SetLimitText(true==b3DES?0x10:0x08);
+	CString cstrCipherKey;
+	pEdit->GetWindowText(cstrCipherKey);
+	pEdit->SetWindowText(cstrCipherKey.Left(true==b3DES?0x10:0x08));
+
 	
-	bool bECB = BST_CHECKED == ((CButton* )(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESECB)))->GetCheck()?true:false;
+	bool bECB = BST_CHECKED == ((CButton*)(GetDlgItem(IDC_RADIO_SYMMETRYDES_DESECB)))->GetCheck()?true:false;
 	GetDlgItem(IDC_EDIT_SYMMETRYDES_INITIALVALUE)->EnableWindow(!bECB);
 }
 
