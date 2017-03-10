42497e3e623f5e3b3211a2688f2060d5c23914b2
arvinchen <arvinchen@22605c27-2254-48e0-b827-bdb5f8ab8418>
2017-03-10 11:29:30 +0000

    link error

--- trunk/Algorithm/EnDecryptIF.cpp
+++ trunk/Algorithm/EnDecryptIF.cpp
@@ -104,7 +104,7 @@ EnDecryptIF::Decrypt(char *dataIn, long dataInlen, char *dataOut)
 bool 
 EnDecryptIF::EncryptDecrypt(ENCRYPTDECRYPT_TYPE eEncrypt, char *dataIn, long dataInlen, char *dataOut)
 {
-	OpenSslSymmetryExample();
+//	OpenSslSymmetryExample();
 	switch(m_eEncrypt) {
 		case ENCRYPT_SYMMETRY:
 			switch(m_eEncryptSymmetry) {
@@ -234,82 +234,83 @@ EnDecryptIF::SetSymmetryDesInitValue(const char* chSymmetryDesInitValue, long lI
 bool 
 EnDecryptIF::OpenSslSymmetryExample()
 {
-	int                                ret,which=1;
-	EVP_CIPHER_CTX             ctx;
-	const EVP_CIPHER      *cipher;
-	unsigned char        key[24],iv[8],in[100],out[108],de[100];
-	int                                i,len,inl,outl,total=0;
-	for(i=0;i<24;i++) {
-		memset(&key[i],i,1);
-	}
-	for(i=0;i<8;i++) {
-		memset(&iv[i],i,1);
-	}
-	for(i=0;i<100;i++) {
-		memset(&in[i],i,1);
-	}
-
-	EVP_CIPHER_CTX_init(&ctx);
-	printf("please select :\n");
-	printf("1: EVP_des_ede3_ofb\n");
-	printf("2: EVP_des_ede3_cbc\n");
-
-	scanf("%d",&which);
-	if(which==1)
-		cipher=EVP_des_ede3_ofb();
-	else
-		cipher=EVP_des_ede3_cbc(); 
-
-	ret=EVP_EncryptInit_ex(&ctx,cipher,NULL,key,iv);
-	if(ret!=1) {
-		printf("EVP_EncryptInit_ex err1!\n");
-		return false;
-	}
-
-	inl=50;
-	len=0;
-	EVP_EncryptUpdate(&ctx,out+len,&outl,in,inl);
-
-	len+=outl;
-	EVP_EncryptUpdate(&ctx,out+len,&outl,in+50,inl);
-
-	len+=outl;
-	EVP_EncryptFinal_ex(&ctx,out+len,&outl);
-
-	len+=outl;
-	printf("加密结果长度：%d\n",len);
-
-	/* 解密 */
-	EVP_CIPHER_CTX_cleanup(&ctx);
-	EVP_CIPHER_CTX_init(&ctx);
-	ret=EVP_DecryptInit_ex(&ctx,cipher,NULL,key,iv);
-
-	if(ret!=1) {
-		printf("EVP_DecryptInit_ex err1!\n");
-		return false;
-	}
-
-	total=0;
-	EVP_DecryptUpdate(&ctx,de+total,&outl,out,44);
-	total+=outl;
-	EVP_DecryptUpdate(&ctx,de+total,&outl,out+44,len-44);
-	total+=outl;
-	ret=EVP_DecryptFinal_ex(&ctx,de+total,&outl);
-	total+=outl;
-	if(ret!=1) {
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		printf("EVP_DecryptFinal_ex err\n");
-		return false;
-	}
-
-	if((total!=100) || (memcmp(de,in,100))) {
-		printf("err!\n");
-		return false;
-	}
-
-	EVP_CIPHER_CTX_cleanup(&ctx);
-	printf("test ok!\n");
-	return true;
+	return false;
+// 	int                                ret,which=1;
+// 	EVP_CIPHER_CTX             ctx;
+// 	const EVP_CIPHER      *cipher;
+// 	unsigned char        key[24],iv[8],in[100],out[108],de[100];
+// 	int                                i,len,inl,outl,total=0;
+// 	for(i=0;i<24;i++) {
+// 		memset(&key[i],i,1);
+// 	}
+// 	for(i=0;i<8;i++) {
+// 		memset(&iv[i],i,1);
+// 	}
+// 	for(i=0;i<100;i++) {
+// 		memset(&in[i],i,1);
+// 	}
+// 
+// 	EVP_CIPHER_CTX_init(&ctx);
+// 	printf("please select :\n");
+// 	printf("1: EVP_des_ede3_ofb\n");
+// 	printf("2: EVP_des_ede3_cbc\n");
+// 
+// 	scanf("%d",&which);
+// 	if(which==1)
+// 		cipher=EVP_des_ede3_ofb();
+// 	else
+// 		cipher=EVP_des_ede3_cbc(); 
+// 
+// 	ret=EVP_EncryptInit_ex(&ctx,cipher,NULL,key,iv);
+// 	if(ret!=1) {
+// 		printf("EVP_EncryptInit_ex err1!\n");
+// 		return false;
+// 	}
+// 
+// 	inl=50;
+// 	len=0;
+// 	EVP_EncryptUpdate(&ctx,out+len,&outl,in,inl);
+// 
+// 	len+=outl;
+// 	EVP_EncryptUpdate(&ctx,out+len,&outl,in+50,inl);
+// 
+// 	len+=outl;
+// 	EVP_EncryptFinal_ex(&ctx,out+len,&outl);
+// 
+// 	len+=outl;
+// 	printf("加密结果长度：%d\n",len);
+// 
+// 	/* 解密 */
+// 	EVP_CIPHER_CTX_cleanup(&ctx);
+// 	EVP_CIPHER_CTX_init(&ctx);
+// 	ret=EVP_DecryptInit_ex(&ctx,cipher,NULL,key,iv);
+// 
+// 	if(ret!=1) {
+// 		printf("EVP_DecryptInit_ex err1!\n");
+// 		return false;
+// 	}
+// 
+// 	total=0;
+// 	EVP_DecryptUpdate(&ctx,de+total,&outl,out,44);
+// 	total+=outl;
+// 	EVP_DecryptUpdate(&ctx,de+total,&outl,out+44,len-44);
+// 	total+=outl;
+// 	ret=EVP_DecryptFinal_ex(&ctx,de+total,&outl);
+// 	total+=outl;
+// 	if(ret!=1) {
+// 		EVP_CIPHER_CTX_cleanup(&ctx);
+// 		printf("EVP_DecryptFinal_ex err\n");
+// 		return false;
+// 	}
+// 
+// 	if((total!=100) || (memcmp(de,in,100))) {
+// 		printf("err!\n");
+// 		return false;
+// 	}
+// 
+// 	EVP_CIPHER_CTX_cleanup(&ctx);
+// 	printf("test ok!\n");
+// 	return true;
 }
 
 bool 
